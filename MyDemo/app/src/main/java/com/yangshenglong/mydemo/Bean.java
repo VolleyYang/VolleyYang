a04eac189d159e0a63f2b28bb10c02213c80d721
VolleyYang <2906787510@qq.com>
2016-11-29 15:51:52 +0800

    ji

--- MyDemo/app/src/main/java/com/yangshenglong/mydemo/Bean.java
+++ MyDemo/app/src/main/java/com/yangshenglong/mydemo/Bean.java
@@ -1,8 +1,11 @@
 package com.yangshenglong.mydemo;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by yangshenglong on 16/11/29.
  */
 
-public class Bean {
+public class Bean extends Fragment {
+
 }
